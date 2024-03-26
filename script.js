76029252e76da05cade445c412f37c20bf24e966
Joshua Mamawag <162310653+jjjmdev@users.noreply.github.com>
2024-03-26 11:11:54 +0800

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+// INIT random computer choice
+// Options are: rock, paper, scissors
+
+let getComputerChoice = () => {
+    let choices = ["ROCK", "PAPER", "SCISSORS"];
+
+    // RETURN either rock, paper, or scissors
+    return choices[Math.floor(Math.random() * 3)];
+};
