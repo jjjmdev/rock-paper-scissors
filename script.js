34d0f22c605393dd4c8e2e6b4a8d805bb47997c9
Joshua Mamawag <162310653+jjjmdev@users.noreply.github.com>
2024-03-26 11:30:34 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -7,3 +7,37 @@ let getComputerChoice = () => {
     // RETURN either rock, paper, or scissors
     return choices[Math.floor(Math.random() * 3)];
 };
+
+// Plays one round
+let playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toUpperCase();
+
+    if (
+        playerSelection !== "ROCK" &&
+        playerSelection !== "PAPER" &&
+        playerSelection !== "SCISSORS"
+    )
+        return "Invalid input.";
+
+    if (playerSelection === computerSelection) return "Draw!";
+
+    if (playerSelection === "ROCK") {
+        if (computerSelection === "PAPER") return "You lose! Paper beats rock.";
+
+        return "You win! Rock beats scissors.";
+    } else if (playerSelection === "PAPER") {
+        if (computerSelection === "SCISSORS")
+            return "You lose! Scissors beat paper";
+
+        return "You win! Paper beats rock.";
+    } else if (computerSelection === "ROCK")
+        // playerSelection = 'scissors' since it's the only remaining option
+        return "You lose! Rock beats scissors.";
+
+    return "You win! Scissors beat paper.";
+};
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
