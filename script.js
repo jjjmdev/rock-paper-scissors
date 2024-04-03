5e0578fcdd918f2301dd5290efd076593635d968
Joshua Mamawag <162310653+jjjmdev@users.noreply.github.com>
2024-04-03 16:05:19 +0800

    Add buttons selection

--- script.js
+++ script.js
@@ -4,66 +4,77 @@ let playerScore = 0;
 let computerScore = 0;
 
 let getComputerChoice = () => {
-    let choices = ["ROCK", "PAPER", "SCISSORS"];
+	let choices = ["ROCK", "PAPER", "SCISSORS"];
 
-    // RETURN either rock, paper, or scissors
-    return choices[Math.floor(Math.random() * 3)];
+	// RETURN either rock, paper, or scissors
+	return choices[Math.floor(Math.random() * 3)];
 };
 
 let getPlayerChoice = () => {
-    return prompt("Rock, paper, or scissors?");
+	return prompt("Rock, paper, or scissors?");
 };
 
 // HELPER
 let playerWin = (playerSelection, computerSelection) => {
-    playerScore++;
-    return `You win! ${playerSelection} beats ${computerSelection}`;
+	playerScore++;
+	return `You win! ${playerSelection} beats ${computerSelection}`;
 };
 
 let computerWin = (playerSelection, computerSelection) => {
-    computerScore++;
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
+	computerScore++;
+	return `You lose! ${computerSelection} beats ${playerSelection}`;
 };
 
 // Plays one round
 let playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toUpperCase();
+	playerSelection = playerSelection.toUpperCase();
 
-    if (
-        playerSelection !== "ROCK" &&
-        playerSelection !== "PAPER" &&
-        playerSelection !== "SCISSORS"
-    )
-        return "Invalid input.";
+	if (
+		playerSelection !== "ROCK" &&
+		playerSelection !== "PAPER" &&
+		playerSelection !== "SCISSORS"
+	)
+		return "Invalid input.";
 
-    if (playerSelection === computerSelection) return "Draw!";
+	if (playerSelection === computerSelection) return "Draw!";
 
-    if (playerSelection === "ROCK") {
-        if (computerSelection === "PAPER") {
-            return computerWin(playerSelection, computerSelection);
-        }
+	if (playerSelection === "ROCK") {
+		if (computerSelection === "PAPER") {
+			return computerWin(playerSelection, computerSelection);
+		}
 
-        return playerWin(playerSelection, computerSelection);
-    } else if (playerSelection === "PAPER") {
-        if (computerSelection === "SCISSORS") {
-            return computerWin(playerSelection, computerSelection);
-        }
+		return playerWin(playerSelection, computerSelection);
+	} else if (playerSelection === "PAPER") {
+		if (computerSelection === "SCISSORS") {
+			return computerWin(playerSelection, computerSelection);
+		}
 
-        return playerWin(playerSelection, computerSelection);
-    } else if (computerSelection === "ROCK") {
-        // playerSelection = 'scissors' since it's the only remaining option
-        return computerWin(playerSelection, computerSelection);
-    } else {
-        return playerWin(playerSelection, computerSelection);
-    }
+		return playerWin(playerSelection, computerSelection);
+	} else if (computerSelection === "ROCK") {
+		// playerSelection = 'scissors' since it's the only remaining option
+		return computerWin(playerSelection, computerSelection);
+	} else {
+		return playerWin(playerSelection, computerSelection);
+	}
 };
 
 let playGame = () => {
-    while (playerScore + computerScore !== 5) {
-        console.log(playRound(getPlayerChoice(), getComputerChoice()));
-    }
+	while (playerScore + computerScore !== 5) {
+		console.log(playRound(getPlayerChoice(), getComputerChoice()));
+	}
 
-    console.log(playerScore, computerScore);
+	console.log(playerScore, computerScore);
 };
 
-playGame();
+const container = document.querySelector("#container");
+container.addEventListener("click", (event) => {
+	let target = event.target;
+
+	switch (target.id) {
+		case "rock":
+		case "paper":
+		case "scissors":
+			console.log(playRound(target.id, getComputerChoice()));
+			break;
+	}
+});
